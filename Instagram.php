c2b55be2f50c3eb0589087bc10762131ea1edbba
mgp25 <me@mgp25.com>
2015-11-16 15:45:26 +0100

    formatting...

--- Instagram.php
+++ Instagram.php
@@ -97,15 +97,15 @@ class Instagram {
   }
 
   protected function generateUUID()
-	{
-		return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-		mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-		mt_rand(0, 0xffff),
-		mt_rand(0, 0x0fff) | 0x4000,
-		mt_rand(0, 0x3fff) | 0x8000,
-		mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-		);
-	}
+  {
+    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+      mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0x0fff) | 0x4000,
+      mt_rand(0, 0x3fff) | 0x8000,
+      mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+    );
+  }
 
   protected function getPostData($filename)
   {
