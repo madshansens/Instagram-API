95f36d2d2680eb7844bc5805713b1e68c1abb757
Felix Schürholz <contact@feelx-software.de>
2016-01-05 16:20:44 +0100

    Fixed Microtime Bug / Added new SIGNATURE KEY / Updated Android and Instagram Version

--- src/Constants.php
+++ src/Constants.php
@@ -6,7 +6,7 @@ class Constants
      * Constant declarations.
      */
      const API_URL         = 'https://i.instagram.com/api/v1/';
-     const USER_AGENT      = 'Instagram 7.10.0 Android (23/6.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)';
-     const IG_SIG_KEY      = 'c1c7d84501d2f0df05c378f5efb9120909ecfb39dff5494aa361ec0deadb509a';
+     const USER_AGENT      = 'Instagram 7.13.1 Android (23/6.0.1; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)';
+     const IG_SIG_KEY      = '8b46309eb680f272cc770d214b7dbe5f0c5d26b6cb82b0b740257360b43618f0';
      const SIG_KEY_VERSION = '4';
 }
