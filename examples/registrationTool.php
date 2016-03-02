972238132792c71d53e187a16f27babfb2f13511
mgp25 <me@mgp25.com>
2016-03-02 16:37:59 -0500

    Applied fixes from StyleCI

--- examples/registrationTool.php
+++ examples/registrationTool.php
@@ -8,21 +8,20 @@ $debug = false;
 
 $r = new InstagramRegistration($debug);
 
-
 echo "###########################\n";
 echo "#                         #\n";
 echo "# Instagram Register Tool #\n";
 echo "#                         #\n";
 echo "###########################\n";
 
-do
-{
-  echo "\n\nUsername: ";
-  $username = trim(fgets(STDIN));
+do {
+    echo "\n\nUsername: ";
+    $username = trim(fgets(STDIN));
 
-  $check = $r->checkUsername($username);
-  if ($check['available'] == false)
-    echo "Username $username not available, try with another one\n\n";
+    $check = $r->checkUsername($username);
+    if ($check['available'] == false) {
+        echo "Username $username not available, try with another one\n\n";
+    }
 } while ($check['available'] == false);
 
 echo "Username $username is available\n\n";
@@ -35,7 +34,6 @@ $email = trim(fgets(STDIN));
 
 $result = $r->createAccount($username, $password, $email);
 
-if (isset($result['account_created']) && ($result['account_created'] == true))
-{
-  echo "Your account was successfully created! :)";
+if (isset($result['account_created']) && ($result['account_created'] == true)) {
+    echo 'Your account was successfully created! :)';
 }
--- src/InstagramRegistration.php
+++ src/InstagramRegistration.php
@@ -3,97 +3,92 @@
 require_once 'Constants.php';
 require_once 'InstagramException.php';
 
-
 class InstagramRegistration
 {
+    protected $debug;
+    protected $IGDataPath;
+    protected $username;
+    protected $uuid;
 
-  protected $debug;
-  protected $IGDataPath;
-  protected $username;
-  protected $uuid;
-
-  public function __construct($debug = false, $IGDataPath = null)
-  {
-    $this->debug = $debug;
-    $this->uuid = $this->generateUUID(true);
-    if (!is_null($IGDataPath))
-      $this->IGDataPath = $IGDataPath;
-    else
-      $this->IGDataPath = __DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR;
-  }
+    public function __construct($debug = false, $IGDataPath = null)
+    {
+        $this->debug = $debug;
+        $this->uuid = $this->generateUUID(true);
+        if (!is_null($IGDataPath)) {
+            $this->IGDataPath = $IGDataPath;
+        } else {
+            $this->IGDataPath = __DIR__.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR;
+        }
+    }
 
   /**
-  * Checks if the username is already taken (exists)
-  *
-  * @param string $username
-  *
-  * @return array
-  *   Username availability data
-  */
+   * Checks if the username is already taken (exists).
+   *
+   * @param string $username
+   *
+   * @return array
+   *   Username availability data
+   */
   public function checkUsername($username)
   {
-      $data = json_encode(array(
-          '_uuid'  => $this->uuid,
+      $data = json_encode([
+          '_uuid'      => $this->uuid,
           'username'   => $username,
-          '_csrftoken' => 'missing'
-      ));
+          '_csrftoken' => 'missing',
+      ]);
 
-      return $this->request("users/check_username/", $this->generateSignature($data))[1];
+      return $this->request('users/check_username/', $this->generateSignature($data))[1];
   }
 
-
   /**
-  * Register account
-  *
-  * @param String $username
-  *
-  * @param String $password
-  *
-  * @param String $email
-  *
-  * @return array
-  *   Registration data
-  */
+   * Register account.
+   *
+   * @param string $username
+   * @param string $password
+   * @param string $email
+   *
+   * @return array
+   *   Registration data
+   */
   public function createAccount($username, $password, $email)
   {
-      $data = json_encode(array(
-          'phone_id'    => $this->uuid,
-          '_csrftoken'  => 'missing',
-          'username'    => $username,
-          'first_name'  => '',
-          'guid'        => $this->uuid,
-          'device_id'   => 'android-' . str_split(md5(mt_rand(1000, 9999)), 17)[mt_rand(0, 1)],
-          'email'       => $email,
+      $data = json_encode([
+          'phone_id'           => $this->uuid,
+          '_csrftoken'         => 'missing',
+          'username'           => $username,
+          'first_name'         => '',
+          'guid'               => $this->uuid,
+          'device_id'          => 'android-'.str_split(md5(mt_rand(1000, 9999)), 17)[mt_rand(0, 1)],
+          'email'              => $email,
           'force_sign_up_code' => '',
-          'qs_stamp'    => '',
-          'password'    => $password
-      ));
+          'qs_stamp'           => '',
+          'password'           => $password,
+      ]);
 
-      $result = $this->request("accounts/create/", $this->generateSignature($data));
-      if (isset($result[1]['account_created']) && ($result[1]['account_created'] == true))
-      {
-        $this->username_id = $result[1]['created_user']['pk'];
-        file_put_contents($this->IGDataPath . "$username-userId.dat", $this->username_id);
-        preg_match('#Set-Cookie: csrftoken=([^;]+)#', $result[0], $match);
-        $token = $match[1];
-        $this->username = $username;
-        file_put_contents($this->IGDataPath . "$username-token.dat", $token);
-        rename($this->IGDataPath . "cookies.dat", $this->IGDataPath . "$username-cookies.dat");
+      $result = $this->request('accounts/create/', $this->generateSignature($data));
+      if (isset($result[1]['account_created']) && ($result[1]['account_created'] == true)) {
+          $this->username_id = $result[1]['created_user']['pk'];
+          file_put_contents($this->IGDataPath."$username-userId.dat", $this->username_id);
+          preg_match('#Set-Cookie: csrftoken=([^;]+)#', $result[0], $match);
+          $token = $match[1];
+          $this->username = $username;
+          file_put_contents($this->IGDataPath."$username-token.dat", $token);
+          rename($this->IGDataPath.'cookies.dat', $this->IGDataPath."$username-cookies.dat");
       }
+
       return $result;
   }
 
+    public function generateSignature($data)
+    {
+        $hash = hash_hmac('sha256', $data, Constants::IG_SIG_KEY);
 
-  public function generateSignature($data)
-  {
-    $hash = hash_hmac('sha256', $data, Constants::IG_SIG_KEY);
+        return 'ig_sig_key_version='.Constants::SIG_KEY_VERSION.'&signed_body='.$hash.'.'.urlencode($data);
+    }
 
-    return 'ig_sig_key_version=' . Constants::SIG_KEY_VERSION . '&signed_body=' . $hash . '.' . urlencode($data);
-  }
-
-  public function generateUUID($type)
-  {
-    $uuid = sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+    public function generateUUID($type)
+    {
+        $uuid = sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
       mt_rand(0, 0xffff), mt_rand(0, 0xffff),
       mt_rand(0, 0xffff),
       mt_rand(0, 0x0fff) | 0x4000,
@@ -101,55 +96,49 @@ class InstagramRegistration
       mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
     );
 
-    return $type ? $uuid : str_replace('-', '', $uuid);
-  }
+        return $type ? $uuid : str_replace('-', '', $uuid);
+    }
 
-  public function request($endpoint, $post = null) {
+    public function request($endpoint, $post = null)
+    {
+        $ch = curl_init();
 
-   $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, Constants::API_URL.$endpoint);
+        curl_setopt($ch, CURLOPT_USERAGENT, Constants::USER_AGENT);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_VERBOSE, false);
+        if (file_exists($this->IGDataPath."$this->username-cookies.dat")) {
+            curl_setopt($ch, CURLOPT_COOKIEFILE, $this->IGDataPath."$this->username-cookies.dat");
+            curl_setopt($ch, CURLOPT_COOKIEJAR, $this->IGDataPath."$this->username-cookies.dat");
+        } else {
+            curl_setopt($ch, CURLOPT_COOKIEFILE, $this->IGDataPath.'cookies.dat');
+            curl_setopt($ch, CURLOPT_COOKIEJAR, $this->IGDataPath.'cookies.dat');
+        }
 
-   curl_setopt($ch, CURLOPT_URL, Constants::API_URL . $endpoint);
-   curl_setopt($ch, CURLOPT_USERAGENT, Constants::USER_AGENT);
-   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-   curl_setopt($ch, CURLOPT_HEADER, true);
-   curl_setopt($ch, CURLOPT_VERBOSE, false);
-   if (file_exists($this->IGDataPath . "$this->username-cookies.dat"))
-   {
-     curl_setopt($ch, CURLOPT_COOKIEFILE, $this->IGDataPath . "$this->username-cookies.dat");
-     curl_setopt($ch, CURLOPT_COOKIEJAR, $this->IGDataPath . "$this->username-cookies.dat");
-   }
-   else {
-     curl_setopt($ch, CURLOPT_COOKIEFILE, $this->IGDataPath . "cookies.dat");
-     curl_setopt($ch, CURLOPT_COOKIEJAR, $this->IGDataPath . "cookies.dat");
-   }
+        if ($post) {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        }
 
-   if ($post) {
+        $resp = curl_exec($ch);
+        $header_len = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $header = substr($resp, 0, $header_len);
+        $body = substr($resp, $header_len);
 
-   curl_setopt($ch, CURLOPT_POST, true);
-   curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_close($ch);
 
-   }
+        if ($this->debug) {
+            echo "REQUEST: $endpoint\n";
+            if (!is_null($post)) {
+                if (!is_array($post)) {
+                    echo "DATA: $post\n";
+                }
+            }
+            echo "RESPONSE: $body\n\n";
+        }
 
-   $resp       = curl_exec($ch);
-   $header_len = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-   $header     = substr($resp, 0, $header_len);
-   $body       = substr($resp, $header_len);
-
-   curl_close($ch);
-
-   if ($this->debug)
-   {
-     echo "REQUEST: $endpoint\n";
-     if (!is_null($post))
-     {
-       if (!is_array($post))
-        echo "DATA: $post\n";
-     }
-     echo "RESPONSE: $body\n\n";
-   }
-
-   return array($header, json_decode($body, true));
-
-  }
+        return [$header, json_decode($body, true)];
+    }
 }
