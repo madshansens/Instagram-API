d7eda0d105b2fb6ff15e757323fa146628361275
mgp25 <me@mgp25.com>
2016-08-17 10:02:43 +0200

    Update CLI example

--- examples/checkpoint.php
+++ examples/checkpoint.php
@@ -3,11 +3,8 @@
 include __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../src/Checkpoint.php';
 
-$username = ''; // Your username
-$settingsPath = null;
-$debug = false;
+$debug = true;
 
-$c = new \InstagramAPI\Checkpoint($username, $settingsPath, $debug);
 
 echo "####################\n";
 echo "#                  #\n";
@@ -15,10 +12,22 @@ echo "#    CHECKPOINT    #\n";
 echo "#                  #\n";
 echo "####################\n";
 
+echo "\n\nYour username: ";
+$username = trim(fgets(STDIN));
+
 if ($username == '') {
     echo "\n\nYou have to set your username\n";
     exit();
 }
+
+echo "\n\nYour settings path folder ([ENTER] if dedault): ";
+$settingsPath = trim(fgets(STDIN));
+
+if ($settingsPath == '') {
+    $settingsPath = null;
+}
+
+$c = new \InstagramAPI\Checkpoint($username, $settingsPath, $debug);
 
 $token = $c->doCheckpoint();
 
