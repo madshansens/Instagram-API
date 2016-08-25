1d88c6f6334a538c2e80a373f818a47a55287798
mgp25 <me@mgp25.com>
2016-08-25 02:11:31 +0200

    added functions
    Will look into a better way in the future

--- src/http/Response/ProfileResponse.php
+++ src/http/Response/ProfileResponse.php
@@ -44,8 +44,93 @@ class ProfileResponse extends Response
         $this->setStatus($response['status']);
     }
 
-    public function getProperty($property){
-        return $this->$property;
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getPhoneNumber()
+    {
+        return $this->phone_number;
+    }
+
+    public function hasAnonymousProfilePicture()
+    {
+        return $this->has_anonymous_profile_picture;
+    }
+
+    public function getHdProfilePicVersions()
+    {
+        return $this->hd_profile_pic_versions;
+    }
+
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    public function getBirthday()
+    {
+        return $this->birthday;
+    }
+
+    public function needsEmailConfirm()
+    {
+        return $this->needs_email_confirm;
+    }
+
+    public function getNationalNumber()
+    {
+        return $this->national_number;
+    }
+
+    public function getProfilePicUrl()
+    {
+        return $this->profile_pic_url;
+    }
+
+    public function getProfilePicId()
+    {
+        return $this->profile_pic_id;
+    }
+
+    public function getBiography()
+    {
+        return $this->biography;
+    }
+
+    public function getFullName()
+    {
+        return $this->full_name;
+    }
+
+    public function getUsernameId()
+    {
+        return $this->pk;
+    }
+
+    public function getCountryCode()
+    {
+        return $this->country_code;
+    }
+
+    public function getHdProfilePicUrlInfo()
+    {
+        return $this->hd_profile_pic_url_info;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function isPrivate()
+    {
+        return $this->is_private;
+    }
+
+    public function getExternalUrl()
+    {
+        return $this->external_url;
     }
 }
-
