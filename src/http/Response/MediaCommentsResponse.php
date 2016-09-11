5ac077c57fa1cdabf0f14b82f91e94f09927c269
Aryan Baghi <ar.baghi.ce@gmail.com>
2016-09-11 11:51:18 +0430

    media comment basic added

--- src/http/Response/MediaCommentsResponse.php
+++ src/http/Response/MediaCommentsResponse.php
@@ -4,22 +4,36 @@ namespace InstagramAPI;
 
 class MediaCommentsResponse extends Response
 {
-    protected $item = null;
+    protected $has_more_comments = false;
+    protected $comments = [];
+    protected $next_max_id = null;
 
     public function __construct($response)
     {
         if (self::STATUS_OK == $response['status']) {
-            if ((isset($response['media'])) && (!empty($response['media']))) {
-                $this->item = new Item($response['media']);
+            if (isset($response['comments'])) {
+                foreach ($response['comments'] as $comment) {
+                    $this->comments[] = new Comment($comment);
+                }    
             }
+            $this->has_more_comments = $response['has_more_comments'];
+            $this->next_max_id = $response['next_max_id'];
         } else {
             $this->setMessage($response['message']);
         }
         $this->setStatus($response['status']);
     }
 
-    public function getItem()
+    public function getNextMaxId()
     {
-        return $this->taken_at;
+        return $this->next_max_id;
+    }
+    public function hasMoreComments()
+    {
+        return $this->has_more_comments;
+    }
+    public function getComments()
+    {
+        return $this->comments;
     }
 }
