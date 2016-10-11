9b6a584a7ddcf0243efc17f4f0bba572eaff5cf6
azureatlas <dario@azureatlas.com>
2016-10-10 21:16:43 -0400

    Update FriendshipStatus.php

--- src/http/Response/Objects/FriendshipStatus.php
+++ src/http/Response/Objects/FriendshipStatus.php
@@ -5,13 +5,22 @@ namespace InstagramAPI;
 class FriendshipStatus
 {
     protected $following;
+    protected $followed_by;
     protected $incoming_request;
     protected $outgoing_request;
     protected $is_private;
+    protected $is_blocking_reel;
+    protected $is_muting_reel;
+    protected $blocking;
+
 
     public function __construct($data)
     {
         $this->following = $data['following'];
+        $this->followed_by = $data['followed_by'];
+        $this->is_blocking_reel = $data['is_blocking_reel'];
+        $this->$is_muting_reel = $data['$is_muting_reel'];
+        $this->$blocking = $data['$blocking'];
         if (array_key_exists('source_token', $data)) {
             $this->incoming_request = $data['incoming_request'];
         }
@@ -28,6 +37,11 @@ class FriendshipStatus
         return $this->following;
     }
 
+    public function getFollowedBy()
+    {
+        return $this->followed_by;
+    }
+
     public function getIncomingRequest()
     {
         return $this->incoming_request;
@@ -42,4 +56,20 @@ class FriendshipStatus
     {
         return $this->is_private;
     }
+    
+    public function isBlockingReel()
+    {
+        return $this->$is_blocking_reel;
+    }
+    
+    public function isMutingReel()
+    {
+        return $this->$is_muting_reel;
+    }
+    
+    public function getBlocking()
+    {
+        return $this->$blocking;
+    }
+
 }
