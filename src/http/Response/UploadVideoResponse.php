d57aa2d76d3319b8b821811911b0a0e590afe047
Yuda Sukmana <yudasukmana1992@gmail.com>
2016-11-01 16:13:52 +0700

    add setter fullresponse for other reponses

--- src/http/Response/UploadVideoResponse.php
+++ src/http/Response/UploadVideoResponse.php
@@ -6,11 +6,12 @@ class UploadVideoResponse extends Response
 {
     protected $upload_id;
     protected $message = null;
-
+    
     public function __construct($response)
     {
         if (self::STATUS_OK == $response['status']) {
             $this->upload_id = $response['upload_id'];
+            $this->setFullResponse($response);
             if (isset($response['message'])) {
                 $this->setMessage($response['message']);
             }
@@ -19,7 +20,7 @@ class UploadVideoResponse extends Response
         }
         $this->setStatus($response['status']);
     }
-
+    
     public function getUploadId()
     {
         return $this->upload_id;
