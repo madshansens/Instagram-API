41e73c9222a94060b7386482c809019c28867f96
Vasiliy-Bondarenko <vabondarenko@gmail.com>
2017-01-28 01:18:04 +0700

    endpoint fix
    Instagram returns redirect from endpoint 'si/fetch_headers' to 'si/fetch_headers/'
    check your logs while login()

--- src/Instagram.php
+++ src/Instagram.php
@@ -125,7 +125,7 @@ class Instagram
         if (!$this->isLoggedIn || $force) {
             $this->syncFeatures(true);
 
-            $response = $this->request('si/fetch_headers')
+            $response = $this->request('si/fetch_headers/')
             ->requireLogin(true)
             ->addParams('challenge_type', 'signup')
             ->addParams('guid', SignatureUtils::generateUUID(false))
