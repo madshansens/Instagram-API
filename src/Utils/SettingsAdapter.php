f6977c8e9ab0a6d24edd70cb9860a06a0f6b700f
Shegl <web-masta@mail.ru>
2017-02-24 17:07:18 +0300

    Update SettingsAdapter.php

--- src/Utils/SettingsAdapter.php
+++ src/Utils/SettingsAdapter.php
@@ -25,10 +25,10 @@ class SettingsAdapter
             $env_host = getenv('DB_HOST');
             $env_name = getenv('DB_NAME');
 
-            $dbUsername = array_key_exists('username', $config) ? $config['username'] : array_key_exists('db_username', $options) ? options['db_username'] : $env_username !== false ? $env_username : null;
-            $password = array_key_exists('password', $config) ? $config['password'] : array_key_exists('db_password', $options) ? options['db_password'] : $env_password !== false ? $env_password : null;
-            $host = array_key_exists('host', $config) ? $config['host'] : array_key_exists('db_host', $options) ? options['db_host'] : $env_host !== false ? $env_host : null;
-            $name = array_key_exists('name', $config) ? $config['name'] : array_key_exists('db_name', $options) ? options['db_name'] : $env_name !== false ? $env_name : null;
+            $dbUsername = array_key_exists('username', $config) ? $config['username'] : array_key_exists('db_username', $options) ? $options['db_username'] : $env_username !== false ? $env_username : null;
+            $password = array_key_exists('password', $config) ? $config['password'] : array_key_exists('db_password', $options) ? $options['db_password'] : $env_password !== false ? $env_password : null;
+            $host = array_key_exists('host', $config) ? $config['host'] : array_key_exists('db_host', $options) ? $options['db_host'] : $env_host !== false ? $env_host : null;
+            $name = array_key_exists('name', $config) ? $config['name'] : array_key_exists('db_name', $options) ? $options['db_name'] : $env_name !== false ? $env_name : null;
 
             $this->setting = new SettingsMysql($username, $dbUsername, $password, $host, $name);
             break;
