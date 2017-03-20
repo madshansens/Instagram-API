a9d7f4136847f7ee7e68f267bbb944b8d83a176e
SteveJobzniak <SteveJobzniak@users.noreply.github.com>
2017-03-20 12:57:32 +0100

    ThrottledExceptions are mandatory, not advisory

--- src/Exception/ThrottledException.php
+++ src/Exception/ThrottledException.php
@@ -4,7 +4,7 @@ namespace InstagramAPI\Exception;
 
 /**
  * Means that you have become throttled by Instagram's API server
- * because of too many requests. You should yourself down!
+ * because of too many requests. You must slow yourself down!
  */
 class ThrottledException extends RequestException
 {
