95c7299edf306f9e284658c9c22c14297cf8eaec
Abyr Valg <valga@abyrga.ru>
2017-04-02 14:25:50 +0300

    ThreadItem: list available types

--- src/Response/Model/ThreadItem.php
+++ src/Response/Model/ThreadItem.php
@@ -4,6 +4,20 @@ namespace InstagramAPI\Response\Model;
 
 class ThreadItem extends \InstagramAPI\Response
 {
+    const PLACEHOLDER = 'placeholder';
+    const TEXT = 'text';
+    const HASHTAG = 'hashtag';
+    const LOCATION = 'location';
+    const PROFILE = 'profile';
+    const MEDIA = 'media';
+    const MEDIA_SHARE = 'media_share';
+    const EXPIRING_MEDIA = 'raven_media';
+    const LIKE = 'like';
+    const ACTION_LOG = 'action_log';
+    const REACTION = 'reaction';
+    const REEL_SHARE = 'reel_share';
+    const LINK = 'link';
+
     /**
      * @var string
      */
