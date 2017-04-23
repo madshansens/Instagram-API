6c6ac80349e7f80879b6f565f43877b8f6c82c44
SteveJobzniak <SteveJobzniak@users.noreply.github.com>
2017-04-23 13:59:11 +0200

    InstagramException: Add ability to hold response
    - Refactors the InstagramException hierarchy from an interface into a
      regular class derived from RuntimeException. Slightly less flexible,
      but allows us to add features to the InstagramException.
    
    - Added the ability to store the server response in the
      InstagramException. This will soon be able to retrieve
      the server response reason for the exception.

--- src/Exception/InstagramException.php
+++ src/Exception/InstagramException.php
@@ -3,10 +3,50 @@
 namespace InstagramAPI\Exception;
 
 /**
- * The core exception interface that ALL other library exceptions derive from.
+ * The core exception that ALL other library exceptions derive from.
  *
  * If you catch this exception, you KNOW it came from our Instagram-API library.
  */
-interface InstagramException
+class InstagramException extends \RuntimeException
 {
+    /**
+     * The full response that triggered the exception, if available.
+     *
+     * @var \InstagramAPI\Response|null
+     */
+    private $_response = null;
+
+    /**
+     * Check whether the exception has a full server response.
+     *
+     * @return bool TRUE if a full response is available, otherwise FALSE.
+     */
+    public function hasResponse()
+    {
+        return $this->_response !== null ? true : false;
+    }
+
+    /**
+     * Get the full server response.
+     *
+     * @return \InstagramAPI\Response|null The full response if one exists,
+     *                                     otherwise NULL.
+     *
+     * @see hasResponse()
+     */
+    public function getResponse()
+    {
+        return $this->_response;
+    }
+
+    /**
+     * Internal. Sets the value of the full server response.
+     *
+     * @param \InstagramAPI\Response|null $response The response value.
+     */
+    public function setResponse(
+        \InstagramAPI\Response $response = null)
+    {
+        $this->_response = $response;
+    }
 }
