d7052aaeb6e06191a3134eb942e07b62fb6714b3
mgp25 <me@mgp25.com>
2017-05-25 15:44:19 +0200

    Comment translation response and model

---
+++ src/Response/Model/CommentTranslations.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace InstagramAPI\Response\Model;
+
+class CommentTranslations extends \InstagramAPI\Response
+{
+    /**
+     * @var string
+     */
+    public $id;
+    public $translation;
+}
---
+++ src/Response/TranslateResponse.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace InstagramAPI\Response;
+
+class TranslateResponse extends \InstagramAPI\Response
+{
+    /**
+     * @var Model\CommentTranslations[]
+     */
+    public $comment_translations;
+}
