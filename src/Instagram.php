052e443a135f313cc3d2657a3f9c61b2d9b7276b
SteveJobzniak <SteveJobzniak@users.noreply.github.com>
2017-05-27 00:08:31 +0200

    Instagram: Refactoring, grouped account functions

--- src/Instagram.php
+++ src/Instagram.php
@@ -143,6 +143,9 @@ class Instagram
      */
     public $experiments;
 
+    /** @var Request\Account Collection of Account related functions. */
+    public $account;
+
     /** @var Request\Live Collection of Live related functions. */
     public $live;
 
@@ -178,6 +181,7 @@ class Instagram
         $this->truncatedDebug = $truncatedDebug;
 
         // Load all function collections.
+        $this->account = new Request\Account($this);
         $this->live = new Request\Live($this);
         $this->direct = new Request\Direct($this);
         $this->media = new Request\Media($this);
@@ -652,6 +656,30 @@ class Instagram
         $this->client->saveCookieJar();
 
         return $response;
+    }
+
+    /**
+     * Change your account's password.
+     *
+     * @param string $oldPassword Old password.
+     * @param string $newPassword New password.
+     *
+     * @throws \InstagramAPI\Exception\InstagramException
+     *
+     * @return \InstagramAPI\Response\ChangePasswordResponse
+     */
+    public function changePassword(
+        $oldPassword,
+        $newPassword)
+    {
+        return $this->request('accounts/change_password/')
+            ->addPost('_uuid', $this->uuid)
+            ->addPost('_uid', $this->account_id)
+            ->addPost('_csrftoken', $this->client->getToken())
+            ->addPost('old_password', $oldPassword)
+            ->addPost('new_password1', $newPassword)
+            ->addPost('new_password2', $newPassword)
+            ->getResponse(new Response\ChangePasswordResponse());
     }
 
     /**
@@ -1626,214 +1654,6 @@ class Instagram
     }
 
     /**
-     * Changes your account's profile picture.
-     *
-     * @param string $photoFilename The photo filename.
-     *
-     * @throws \InstagramAPI\Exception\InstagramException
-     *
-     * @return \InstagramAPI\Response\Model\User
-     */
-    public function changeProfilePicture(
-        $photoFilename)
-    {
-        return $this->client->changeProfilePicture($photoFilename);
-    }
-
-    /**
-     * Remove your account's profile picture.
-     *
-     * @throws \InstagramAPI\Exception\InstagramException
-     *
-     * @return \InstagramAPI\Response\UserInfoResponse
-     */
-    public function removeProfilePicture()
-    {
-        return $this->request('accounts/remove_profile_picture/')
-        ->addPost('_uuid', $this->uuid)
-        ->addPost('_uid', $this->account_id)
-        ->addPost('_csrftoken', $this->client->getToken())
-        ->getResponse(new Response\UserInfoResponse());
-    }
-
-    /**
-     * Sets your account to private.
-     *
-     * @throws \InstagramAPI\Exception\InstagramException
-     *
-     * @return \InstagramAPI\Response\UserInfoResponse
-     */
-    public function setPrivateAccount()
-    {
-        return $this->request('accounts/set_private/')
-        ->addPost('_uuid', $this->uuid)
-        ->addPost('_uid', $this->account_id)
-        ->addPost('_csrftoken', $this->client->getToken())
-        ->getResponse(new Response\UserInfoResponse());
-    }
-
-    /**
-     * Sets your account to public.
-     *
-     * @throws \InstagramAPI\Exception\InstagramException
-     *
-     * @return \InstagramAPI\Response\UserInfoResponse
-     */
-    public function setPublicAccount()
-    {
-        return $this->request('accounts/set_public/')
-        ->addPost('_uuid', $this->uuid)
-        ->addPost('_uid', $this->account_id)
-        ->addPost('_csrftoken', $this->client->getToken())
-        ->getResponse(new Response\UserInfoResponse());
-    }
-
-    /**
-     * Get details about the currently logged in account.
-     *
-     * @throws \InstagramAPI\Exception\InstagramException
-     *
-     * @return \InstagramAPI\Response\UserInfoResponse
-     */
-    public function getCurrentUser()
-    {
-        return $this->request('accounts/current_user/')
-        ->addParams('edit', true)
-        ->addPost('_uuid', $this->uuid)
-        ->addPost('_uid', $this->account_id)
-        ->addPost('_csrftoken', $this->client->getToken())
-        ->getResponse(new Response\UserInfoResponse());
-    }
-
-    /**
-     * Edit your profile.
-     *
-     * @param string $url       Website URL. Use "" for nothing.
-     * @param string $phone     Phone number. Use "" for nothing.
-     * @param string $firstName Name. Use "" for nothing.
-     * @param string $biography Biography text. Use "" for nothing.
-     * @param string $email     Email. Required.
-     * @param int    $gender    Gender. Male = 1, Female = 2, Unknown = 3.
-     *
-     * @throws \InstagramAPI\Exception\InstagramException
-     *
-     * @return \InstagramAPI\Response\UserInfoResponse
-     */
-    public function editProfile(
-        $url,
-        $phone,
-        $firstName,
-        $biography,
-        $email,
-        $gender)
-    {
-        return $this->request('accounts/edit_profile/')
-        ->addPost('_uuid', $this->uuid)
-        ->addPost('_uid', $this->account_id)
-        ->addPost('_csrftoken', $this->client->getToken())
-        ->addPost('external_url', $url)
-        ->addPost('phone_number', $phone)
-        ->addPost('username', $this->username)
-        ->addPost('first_name', $firstName)
-        ->addPost('biography', $biography)
-        ->addPost('email', $email)
-        ->addPost('gender', $gender)
-        ->getResponse(new Response\UserInfoResponse());
-    }
-
-    /**
-     * Change your account's password.
-     *
-     * @param string $oldPassword Old password.
-     * @param string $newPassword New password.
-     *
-     * @throws \InstagramAPI\Exception\InstagramException
-     *
-     * @return \InstagramAPI\Response\ChangePasswordResponse
-     */
-    public function changePassword(
-        $oldPassword,
-        $newPassword)
-    {
-        return $this->request('accounts/change_password/')
-        ->addPost('_uuid', $this->uuid)
-        ->addPost('_uid', $this->account_id)
-        ->addPost('_csrftoken', $this->client->getToken())
-        ->addPost('old_password', $oldPassword)
-        ->addPost('new_password1', $newPassword)
-        ->addPost('new_password2', $newPassword)
-        ->getResponse(new Response\ChangePasswordResponse());
-    }
-
-    /**
-     * Get account spam filter status.
-     *
-     * @throws \InstagramAPI\Exception\InstagramException
-     *
-     * @return \InstagramAPI\Response\CommentFilterResponse
-     */
-    public function getCommentFilter()
-    {
-        return $this->request('accounts/get_comment_filter/')
-            ->getResponse(new Response\CommentFilterResponse());
-    }
-
-    /**
-     * Get account spam filter keywords.
-     *
-     * @throws \InstagramAPI\Exception\InstagramException
-     *
-     * @return \InstagramAPI\Response\CommentFilterKeywordsResponse
-     */
-    public function getCommentFilterKeywords()
-    {
-        return $this->request('accounts/get_comment_filter_keywords/')
-            ->getResponse(new Response\CommentFilterKeywordsResponse());
-    }
-
-    /**
-     * Set account spam filter status (on/off).
-     *
-     * @param int $config_value Whether spam filter is on (0 or 1).
-     *
-     * @throws \InstagramAPI\Exception\InstagramException
-     *
-     * @return \InstagramAPI\Response\CommentFilterSetResponse
-     */
-    public function setCommentFilter(
-        $config_value)
-    {
-        return $this->request('accounts/set_comment_filter/')
-            ->addPost('_uuid', $this->uuid)
-            ->addPost('_uid', $this->account_id)
-            ->addPost('_csrftoken', $this->client->getToken())
-            ->addPost('config_value', $config_value)
-            ->setSignedPost(true)
-            ->getResponse(new Response\CommentFilterSetResponse());
-    }
-
-    /**
-     * Set account spam filter keywords.
-     *
-     * @param string $keywords List of blocked words, separated by comma.
-     *
-     * @throws \InstagramAPI\Exception\InstagramException
-     *
-     * @return \InstagramAPI\Response\CommentFilterSetResponse
-     */
-    public function setCommentFilterKeywords(
-        $keywords)
-    {
-        return $this->request('accounts/set_comment_filter_keywords/')
-            ->addPost('_uuid', $this->uuid)
-            ->addPost('_uid', $this->account_id)
-            ->addPost('_csrftoken', $this->client->getToken())
-            ->addPost('keywords', $keywords)
-            ->setSignedPost(true)
-            ->getResponse(new Response\CommentFilterSetResponse());
-    }
-
-    /**
      * Get recent activity.
      *
      * @throws \InstagramAPI\Exception\InstagramException
@@ -2007,7 +1827,7 @@ class Instagram
     /**
      * Get user details about your own account.
      *
-     * Also try getCurrentUser() instead, for even more details.
+     * Also try account->getCurrentUser() instead, for even more details.
      *
      * @throws \InstagramAPI\Exception\InstagramException
      *
@@ -2348,30 +2168,6 @@ class Instagram
         $requestData = $this->request('friendships/pending/');
 
         return $requestData->getResponse(new Response\FollowerAndFollowingResponse());
-    }
-
-    /**
-     * Set your account's first name and phone (optional).
-     *
-     * @param string $name  Your first name.
-     * @param string $phone Your phone number (optional).
-     *
-     * @throws \InstagramAPI\Exception\InstagramException
-     *
-     * @return \InstagramAPI\Response
-     */
-    public function setNameAndPhone(
-        $name = '',
-        $phone = '')
-    {
-        return $this->request('accounts/set_phone_and_name/')
-        ->setSignedPost(true)
-        ->addPost('_uuid', $this->uuid)
-        ->addPost('_uid', $this->account_id)
-        ->addPost('_csrftoken', $this->client->getToken())
-        ->addPost('first_name', $name)
-        ->addPost('phone_number', $phone)
-        ->getResponse(new \InstagramAPI\Response());
     }
 
     /**
