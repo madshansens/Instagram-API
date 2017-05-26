410939f6ab81e825f8ddc92866171dcdfee0e8b7
mgp25 <me@mgp25.com>
2017-05-26 15:06:54 +0200

    PushPreferencesResponse and model

---
+++ src/Response/PushPreferencesResponse.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace InstagramAPI\Response;
+
+class PushPreferencesResponse extends \InstagramAPI\Response
+{
+    /**
+     * @var Model\PushSettings[]
+     */
+    public $push_settings;
+}
