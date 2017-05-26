bca657af0aee301baf5f06df82b6cd532295380a
SteveJobzniak <SteveJobzniak@users.noreply.github.com>
2017-05-27 00:38:53 +0200

    Instagram: Finished refactoring, revert account
    It's way too hard to group all functions into sub-spaces. So I've
    decided to undo the "account" group, and cancel the idea of a "people"
    group (for users/friendships).
    
    Instead, the sub-spaces have already solved the design goal:
    
    - We now have sub-spaces for related functions for Direct messaging,
      Live broadcasts, Media actions, Story actions and Timeline actions.
    
    - We can finally name functions in those sub-spaces very clearly, since
      they will not clash with anything outside.
    
    They're now easy to find, in clean sub-spaces, such as:
    
    $ig->story->uploadPhoto();
    $ig->timeline->uploadVideo();
    $ig->direct->sendText();
    
    But it's nearly impossible to group ALL other class actions into
    subgroups too, because things like "login()" belongs to "Account"
    related actions, but we obviously want that on the main class.
    
    So we have to draw the line somewhere. And I draw it here. The
    user-related and account-related functions are way too many, and way too
    inconsistent to be able to easily divide them into groups.
    
    Those will stay in the main class. Other contributors are welcome to
    create more groups, with lots of thought and care. But it's fine that we
    only created five major sub-groups (Direct, Live, Media, Story and
    Timeline) and leave the rest in the core.
    
    We've shrunken the core from ~5000 lines to ~3000 lines, and made the
    functions easier to find and better named, so that's a win already!

--- src/Instagram.php
+++ src/Instagram.php
@@ -143,9 +143,6 @@ class Instagram
      */
     public $experiments;
 
-    /** @var Request\Account Collection of Account related functions. */
-    public $account;
-
     /** @var Request\Live Collection of Live related functions. */
     public $live;
 
@@ -181,7 +178,6 @@ class Instagram
         $this->truncatedDebug = $truncatedDebug;
 
         // Load all function collections.
-        $this->account = new Request\Account($this);
         $this->live = new Request\Live($this);
         $this->direct = new Request\Direct($this);
         $this->media = new Request\Media($this);
@@ -849,6 +845,214 @@ class Instagram
 
             return $result;
         }
+    }
+
+    /**
+     * Get details about the currently logged in account.
+     *
+     * @throws \InstagramAPI\Exception\InstagramException
+     *
+     * @return \InstagramAPI\Response\UserInfoResponse
+     */
+    public function getCurrentUser()
+    {
+        return $this->request('accounts/current_user/')
+            ->addParams('edit', true)
+            ->addPost('_uuid', $this->uuid)
+            ->addPost('_uid', $this->account_id)
+            ->addPost('_csrftoken', $this->client->getToken())
+            ->getResponse(new Response\UserInfoResponse());
+    }
+
+    /**
+     * Edit your profile.
+     *
+     * @param string $url       Website URL. Use "" for nothing.
+     * @param string $phone     Phone number. Use "" for nothing.
+     * @param string $firstName Name. Use "" for nothing.
+     * @param string $biography Biography text. Use "" for nothing.
+     * @param string $email     Email. Required.
+     * @param int    $gender    Gender. Male = 1, Female = 2, Unknown = 3.
+     *
+     * @throws \InstagramAPI\Exception\InstagramException
+     *
+     * @return \InstagramAPI\Response\UserInfoResponse
+     */
+    public function editProfile(
+        $url,
+        $phone,
+        $firstName,
+        $biography,
+        $email,
+        $gender)
+    {
+        return $this->request('accounts/edit_profile/')
+            ->addPost('_uuid', $this->uuid)
+            ->addPost('_uid', $this->account_id)
+            ->addPost('_csrftoken', $this->client->getToken())
+            ->addPost('external_url', $url)
+            ->addPost('phone_number', $phone)
+            ->addPost('username', $this->username)
+            ->addPost('first_name', $firstName)
+            ->addPost('biography', $biography)
+            ->addPost('email', $email)
+            ->addPost('gender', $gender)
+            ->getResponse(new Response\UserInfoResponse());
+    }
+
+    /**
+     * Set your account's first name and phone.
+     *
+     * @param string $name  Your first name.
+     * @param string $phone Your phone number (optional).
+     *
+     * @throws \InstagramAPI\Exception\InstagramException
+     *
+     * @return \InstagramAPI\Response
+     */
+    public function setNameAndPhone(
+        $name = '',
+        $phone = '')
+    {
+        return $this->request('accounts/set_phone_and_name/')
+            ->setSignedPost(true)
+            ->addPost('_uuid', $this->uuid)
+            ->addPost('_uid', $this->account_id)
+            ->addPost('_csrftoken', $this->client->getToken())
+            ->addPost('first_name', $name)
+            ->addPost('phone_number', $phone)
+            ->getResponse(new \InstagramAPI\Response());
+    }
+
+    /**
+     * Changes your account's profile picture.
+     *
+     * @param string $photoFilename The photo filename.
+     *
+     * @throws \InstagramAPI\Exception\InstagramException
+     *
+     * @return \InstagramAPI\Response\Model\User
+     */
+    public function changeProfilePicture(
+        $photoFilename)
+    {
+        return $this->client->changeProfilePicture($photoFilename);
+    }
+
+    /**
+     * Remove your account's profile picture.
+     *
+     * @throws \InstagramAPI\Exception\InstagramException
+     *
+     * @return \InstagramAPI\Response\UserInfoResponse
+     */
+    public function removeProfilePicture()
+    {
+        return $this->request('accounts/remove_profile_picture/')
+            ->addPost('_uuid', $this->uuid)
+            ->addPost('_uid', $this->account_id)
+            ->addPost('_csrftoken', $this->client->getToken())
+            ->getResponse(new Response\UserInfoResponse());
+    }
+
+    /**
+     * Sets your account to public.
+     *
+     * @throws \InstagramAPI\Exception\InstagramException
+     *
+     * @return \InstagramAPI\Response\UserInfoResponse
+     */
+    public function setPublicAccount()
+    {
+        return $this->request('accounts/set_public/')
+            ->addPost('_uuid', $this->uuid)
+            ->addPost('_uid', $this->account_id)
+            ->addPost('_csrftoken', $this->client->getToken())
+            ->getResponse(new Response\UserInfoResponse());
+    }
+
+    /**
+     * Sets your account to private.
+     *
+     * @throws \InstagramAPI\Exception\InstagramException
+     *
+     * @return \InstagramAPI\Response\UserInfoResponse
+     */
+    public function setPrivateAccount()
+    {
+        return $this->request('accounts/set_private/')
+            ->addPost('_uuid', $this->uuid)
+            ->addPost('_uid', $this->account_id)
+            ->addPost('_csrftoken', $this->client->getToken())
+            ->getResponse(new Response\UserInfoResponse());
+    }
+
+    /**
+     * Get account spam filter status.
+     *
+     * @throws \InstagramAPI\Exception\InstagramException
+     *
+     * @return \InstagramAPI\Response\CommentFilterResponse
+     */
+    public function getCommentFilter()
+    {
+        return $this->request('accounts/get_comment_filter/')
+            ->getResponse(new Response\CommentFilterResponse());
+    }
+
+    /**
+     * Set account spam filter status (on/off).
+     *
+     * @param int $config_value Whether spam filter is on (0 or 1).
+     *
+     * @throws \InstagramAPI\Exception\InstagramException
+     *
+     * @return \InstagramAPI\Response\CommentFilterSetResponse
+     */
+    public function setCommentFilter(
+        $config_value)
+    {
+        return $this->request('accounts/set_comment_filter/')
+            ->addPost('_uuid', $this->uuid)
+            ->addPost('_uid', $this->account_id)
+            ->addPost('_csrftoken', $this->client->getToken())
+            ->addPost('config_value', $config_value)
+            ->setSignedPost(true)
+            ->getResponse(new Response\CommentFilterSetResponse());
+    }
+
+    /**
+     * Get account spam filter keywords.
+     *
+     * @throws \InstagramAPI\Exception\InstagramException
+     *
+     * @return \InstagramAPI\Response\CommentFilterKeywordsResponse
+     */
+    public function getCommentFilterKeywords()
+    {
+        return $this->request('accounts/get_comment_filter_keywords/')
+            ->getResponse(new Response\CommentFilterKeywordsResponse());
+    }
+
+    /**
+     * Set account spam filter keywords.
+     *
+     * @param string $keywords List of blocked words, separated by comma.
+     *
+     * @throws \InstagramAPI\Exception\InstagramException
+     *
+     * @return \InstagramAPI\Response\CommentFilterSetResponse
+     */
+    public function setCommentFilterKeywords(
+        $keywords)
+    {
+        return $this->request('accounts/set_comment_filter_keywords/')
+            ->addPost('_uuid', $this->uuid)
+            ->addPost('_uid', $this->account_id)
+            ->addPost('_csrftoken', $this->client->getToken())
+            ->addPost('keywords', $keywords)
+            ->setSignedPost(true)
+            ->getResponse(new Response\CommentFilterSetResponse());
     }
 
     /**
@@ -1827,7 +2031,7 @@ class Instagram
     /**
      * Get user details about your own account.
      *
-     * Also try account->getCurrentUser() instead, for even more details.
+     * Also try getCurrentUser() instead, for even more details.
      *
      * @throws \InstagramAPI\Exception\InstagramException
      *
