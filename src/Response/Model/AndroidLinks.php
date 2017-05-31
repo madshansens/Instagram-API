4cde441c01913379a415742e9979c939d1bcf9f8
mgp25 <me@mgp25.com>
2017-06-01 01:51:04 +0200

    Response/Model/AndroidLinks.php: New model added

---
+++ src/Response/Model/AndroidLinks.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace InstagramAPI\Response\Model;
+
+class AndroidLinks extends \InstagramAPI\Response
+{
+    public $linkType;
+    public $webUri;
+    public $androidClass;
+    public $package;
+    public $deeplinkUri;
+    public $callToActionTitle;
+}
