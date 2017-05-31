a43e26002f438a77d24f5e0275e7dc663eb6054e
mgp25 <me@mgp25.com>
2017-06-01 01:51:31 +0200

    Response/Model/ActionBadge.php: New model added

---
+++ src/Response/Model/ActionBadge.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace InstagramAPI\Response\Model;
+
+class ActionBadge extends \InstagramAPI\Response
+{
+    public $action_type;
+    public $action_count;
+    public $action_timestamp;
+}
