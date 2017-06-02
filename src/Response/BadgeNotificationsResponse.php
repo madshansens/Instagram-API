707929e6eb2e8a45989237ca3e4aaac1e24ee16c
mgp25 <me@mgp25.com>
2017-06-02 20:49:46 +0200

    Update /Response/BadgeNotificationResponse.php: Clarification added

--- src/Response/BadgeNotificationsResponse.php
+++ src/Response/BadgeNotificationsResponse.php
@@ -4,5 +4,5 @@ namespace InstagramAPI\Response;
 
 class BadgeNotificationsResponse extends \InstagramAPI\Response
 {
-    public $badge_payload;
+    public $badge_payload; // Only exists if you have notifications, contains data keyed by userId.
 }
