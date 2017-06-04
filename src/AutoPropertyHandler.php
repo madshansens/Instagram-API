2b05fc972045e5fc9714df6b4a3e174fb33151e3
SteveJobzniak <SteveJobzniak@users.noreply.github.com>
2017-06-04 12:23:24 +0200

    AutoPropertyHandler: Add support for camelcase var
    - Adds support for camelcase variables, such as $iTunesItem.
    
    - Adds support for variables with leading underscores, such as $_messages.
    
    Now we handle all types of variable names that Instagram uses.

--- src/AutoPropertyHandler.php
+++ src/AutoPropertyHandler.php
@@ -13,6 +13,16 @@ namespace InstagramAPI;
  * format, which is "$some_value". That object property can then be magically
  * accessed via "getSomeValue()", "setSomeValue()" and "isSomeValue()".
  *
+ * Examples (normal lowercase properties separated by underscores):
+ * public $location; = getLocation(); isLocation(); setLocation();
+ * public $is_valid; = getIsValid(); isIsValid(); setIsValid();
+ *
+ * Examples (rare properties with a leading underscore):
+ * public $_messages; = get_Messages(); is_Messages(); set_Messages();
+ *
+ * Examples (rare camelcase properties):
+ * public $iTunesItem; = getITunesItem(); isITunesItem(); setITunesItem();
+ *
  * @author SteveJobzniak (https://github.com/SteveJobzniak)
  */
 class AutoPropertyHandler
@@ -21,6 +31,11 @@ class AutoPropertyHandler
      * __CALL is invoked when attempting to access missing functions.
      *
      * This handler auto-maps setters and getters for object properties.
+     * 
+     * NOTE: Don't worry about performance. This whole function takes about
+     * 0.025 MILLIseconds per property, which actually compares very favorably
+     * to native accessor functions (which take about 0.002 MILLIseconds per
+     * call).
      *
      * @param string $functionName Name of the method being called.
      * @param array  $arguments    Array of arguments passed to the method.
@@ -43,7 +58,23 @@ class AutoPropertyHandler
 
         // Determine the type (such as "get") and the property (ie "is_valid").
         $functionType = array_shift($chunks);
-        $propertyName = implode('_', $chunks);
+        $propertyName = implode('_', $chunks); // "is_valid"
+
+        // Some objects have rare camelcase properties, so instead of naming it
+        // "i_tunes_item" they have "iTunesItem" as the property. We'll have to
+        // check for the existence of a camelcase property and use it if found.
+        if (($len = count($chunks)) >= 2) {
+            // Make word 2 and higher start with uppercase ("i,Tunes,Item").
+            // NOTE: Instagram's rule so far is that the first word is always
+            // lowercase when they use camelcase.
+            for ($i = 1, $len = count($chunks); $i < $len; ++$i) {
+                $chunks[$i] = ucfirst($chunks[$i]);
+            }
+            $camelPropertyName = implode('', $chunks); // "iTunesItem"
+            if (property_exists($this, $camelPropertyName)) {
+                $propertyName = $camelPropertyName; // Use this name instead.
+            }
+        }
 
         // Make sure the requested function has a corresponding object property.
         if (!property_exists($this, $propertyName)) {
@@ -94,6 +125,15 @@ class AutoPropertyHandler
         // contain at most a SINGLE capital letter (the chunk's first letter).
         foreach ($chunks as &$chunk) {
             $chunk = lcfirst($chunk); // Only first letter may be uppercase.
+        }
+
+        // If there are 2+ chunks and the first (the function type) ends with a
+        // trailing underscore, it means that they wanted to access a property
+        // beginning with an underscore, so move it to the start of the 2nd
+        // ("property name") chunk instead.
+        if (count($chunks) >= 2 && substr($chunks[0], -1) == '_') {
+            $chunks[0] = substr($chunks[0], 0, -1); // "get_" => "get".
+            $chunks[1] = '_'.$chunks[1]; // "messages" => "_messages".
         }
 
         return $chunks;
