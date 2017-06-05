db73f2c078f0d38453a5d1b8b88072d4cc939e35
mgp25 <me@mgp25.com>
2017-06-05 11:45:49 +0200

    Response/Model/Aymf.php: New model

---
+++ src/Response/Model/Aymf.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace InstagramAPI\Response\Model;
+
+use InstagramAPI\AutoPropertyHandler;
+
+class Aymf extends AutoPropertyHandler
+{
+    /**
+     * @var Item[]
+     */
+    public $items;
+    public $more_available;
+}
