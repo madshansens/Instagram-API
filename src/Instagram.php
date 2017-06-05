5488aa09bfa74b6e698fc0a490025b68919fc80b
SteveJobzniak <SteveJobzniak@users.noreply.github.com>
2017-06-05 16:39:56 +0200

    Create new Request/Creative request collection
    This was something I thought about earlier while grouping, but since we
    only had one I placed it in "Story".
    
    Now we're getting another creative-related endpoint, so it's definitely
    more appropriate to give all of those their own request collection!

--- src/Instagram.php
+++ src/Instagram.php
@@ -147,6 +147,8 @@ class Instagram
     public $account;
     /** @var Request\Business Collection of Business related functions. */
     public $business;
+    /** @var Request\Creative Collection of Creative related functions. */
+    public $creative;
     /** @var Request\Direct Collection of Direct related functions. */
     public $direct;
     /** @var Request\Discover Collection of Discover related functions. */
@@ -194,6 +196,7 @@ class Instagram
         // Load all function collections.
         $this->account = new Request\Account($this);
         $this->business = new Request\Business($this);
+        $this->creative = new Request\Creative($this);
         $this->direct = new Request\Direct($this);
         $this->discover = new Request\Discover($this);
         $this->hashtag = new Request\Hashtag($this);
