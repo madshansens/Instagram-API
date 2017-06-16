7f22d673621af142242846e5ad67d351a9497e41
mgp25 <me@mgp25.com>
2017-06-16 19:53:37 +0200

    Response/GetListsResponse.php: New response

---
+++ src/Response/GetListsResponse.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace InstagramAPI\Response;
+
+use InstagramAPI\AutoPropertyHandler;
+use InstagramAPI\ResponseInterface;
+use InstagramAPI\ResponseTrait;
+
+/**
+ * @method mixed getAutoLoadMoreEnabled()
+ * @method Model\Item[] getItems()
+ * @method mixed getMoreAvailable()
+ * @method bool isAutoLoadMoreEnabled()
+ * @method bool isItems()
+ * @method bool isMoreAvailable()
+ * @method setAutoLoadMoreEnabled(mixed $value)
+ * @method setItems(Model\Item[] $value)
+ * @method setMoreAvailable(mixed $value)
+ */
+class GetListsResponse extends AutoPropertyHandler implements ResponseInterface
+{
+    use ResponseTrait;
+
+    /**
+     * @var Model\Item[]
+     */
+    public $items;
+    public $more_available;
+    public $auto_load_more_enabled;
+}
