72eaded805abc60c5054a4ad0055ff0930f7f4c9
mgp25 <me@mgp25.com>
2017-06-29 21:02:00 +0200

    Response/ClientEventLogsResponse: New response
    Testing function yet.

---
+++ src/Response/ClientEventLogsResponse.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace InstagramAPI\Response;
+
+use InstagramAPI\AutoPropertyHandler;
+use InstagramAPI\ResponseInterface;
+use InstagramAPI\ResponseTrait;
+
+class ClientEventLogsResponse extends AutoPropertyHandler implements ResponseInterface
+{
+    use ResponseTrait;
+
+    public $checksum;
+    public $config;
+    public $app_data;
+}
