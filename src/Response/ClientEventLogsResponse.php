dde91eab423f7547f99ff8a4cdd250806b146acd
mgp25 <me@mgp25.com>
2017-06-30 12:31:23 +0200

    Response/ClientEventLogsResponse: Adding doc

--- src/Response/ClientEventLogsResponse.php
+++ src/Response/ClientEventLogsResponse.php
@@ -6,6 +6,20 @@ use InstagramAPI\AutoPropertyHandler;
 use InstagramAPI\ResponseInterface;
 use InstagramAPI\ResponseTrait;
 
+/**
+ * @method mixed getAppData()
+ * @method mixed getChecksum()
+ * @method mixed getConfig()
+ * @method mixed getError()
+ * @method bool isAppData()
+ * @method bool isChecksum()
+ * @method bool isConfig()
+ * @method bool isError()
+ * @method setAppData(mixed $value)
+ * @method setChecksum(mixed $value)
+ * @method setConfig(mixed $value)
+ * @method setError(mixed $value)
+ */
 class ClientEventLogsResponse extends AutoPropertyHandler implements ResponseInterface
 {
     use ResponseTrait;
