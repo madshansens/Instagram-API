aa07a1ae8ac8d616a8254ae549c1a52bfa623354
Abyr Valg <valga@abyrga.ru>
2017-07-06 11:02:15 +0300

    10.24.0 - client_request_id

--- src/Response/Model/DirectSendItemPayload.php
+++ src/Response/Model/DirectSendItemPayload.php
@@ -5,25 +5,32 @@ namespace InstagramAPI\Response\Model;
 use InstagramAPI\AutoPropertyHandler;
 
 /**
- * @method mixed getClientContext()
+ * @method string getClientContext()
+ * @method string getClientRequestId()
  * @method string getItemId()
- * @method mixed getMessage()
+ * @method string getMessage()
  * @method string getThreadId()
  * @method string getTimestamp()
  * @method bool isClientContext()
+ * @method bool isClientRequestId()
  * @method bool isItemId()
  * @method bool isMessage()
  * @method bool isThreadId()
  * @method bool isTimestamp()
- * @method setClientContext(mixed $value)
+ * @method setClientContext(string $value)
+ * @method setClientRequestId(string $value)
  * @method setItemId(string $value)
- * @method setMessage(mixed $value)
+ * @method setMessage(string $value)
  * @method setThreadId(string $value)
  * @method setTimestamp(string $value)
  */
 class DirectSendItemPayload extends AutoPropertyHandler
 {
+    /** @var string */
+    public $client_request_id;
+    /** @var string */
     public $client_context;
+    /** @var string */
     public $message;
     /** @var string */
     public $item_id;
