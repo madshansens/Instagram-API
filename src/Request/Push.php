4f347830ade10a675966f48c4f615a4e5bf034ae
mgp25 <me@mgp25.com>
2017-07-31 09:17:24 +0200

    Request/Push: Update register()

--- src/Request/Push.php
+++ src/Request/Push.php
@@ -10,35 +10,51 @@ use InstagramAPI\Response;
 class Push extends RequestCollection
 {
     /**
-     * Register to the MQTT push server.
+     * Register to the MQTT or GCM push server.
      *
-     * @param $gcmToken
+     * @param string $pushChannel The channel you want to register, it can be mqtt or gcm.
+     * @param string $token       The token used to register to the push channel.
      *
+     * @throws \InvalidArgumentException
      * @throws \InstagramAPI\Exception\InstagramException
      *
      * @return \InstagramAPI\Response\PushRegisterResponse
      */
     public function register(
-        $gcmToken)
+        $pushChannel,
+        $token)
     {
-        $deviceToken = json_encode([
-            'k' => $gcmToken,
-            'v' => 0,
-            't' => 'fbns-b64',
-        ]);
+        // Make sure we only allow these for push channels.
+        if ($pushChannel != 'mqtt' && $pushChannel != 'gcm') {
+            throw new \InvalidArgumentException(sprintf('Bad push channel "%s".', $pushChannel));
+        }
 
-        return $this->ig->request('push/register/')
-            ->addParam('platform', '10')
-            ->addParam('device_type', 'android_mqtt')
-            ->addPost('_uuid', $this->ig->uuid)
-            ->addPost('guid', $this->ig->uuid)
+        $request = $this->ig->request('push/register/')
             ->addPost('phone_id', $this->ig->settings->get('phone_id'))
-            ->addPost('device_type', 'android_mqtt')
-            ->addPost('device_token', $deviceToken)
-            ->addPost('is_main_push_channel', true)
             ->addPost('_csrftoken', $this->ig->client->getToken())
-            ->addPost('users', $this->ig->account_id)
-            ->getResponse(new Response\PushRegisterResponse());
+            ->addPost('guid', $this->ig->uuid)
+            ->addPost('_uuid', $this->ig->uuid)
+            ->addPost('users', $this->ig->account_id);
+
+        if ($pushChannel == 'gcm') {
+            $request
+                ->addPost('is_main_push_channel', false)
+                ->addPost('device_type', 'android_gcm')
+                ->addPost('device_token', $token);
+        } elseif ($pushChannel == 'mqtt') {
+            $deviceToken = json_encode([
+                'k' => $token,
+                'v' => 0,
+                't' => 'fbns-b64',
+            ]);
+
+            $request
+                ->addPost('is_main_push_channel', true)
+                ->addPost('device_type', 'android_mqtt')
+                ->addPost('device_token', $deviceToken);
+        }
+
+        return $request->getResponse(new Response\PushRegisterResponse());
     }
 
     /**
