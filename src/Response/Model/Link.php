9d116cc726a5ef96539b514bffe5a9d5c8da51c8
mgp25 <me@mgp25.com>
2017-08-02 13:27:36 +0200

    Response/Model/Link: Add new properties

--- src/Response/Model/Link.php
+++ src/Response/Model/Link.php
@@ -7,15 +7,21 @@ use InstagramAPI\AutoPropertyHandler;
 /**
  * @method mixed getEnd()
  * @method string getId()
+ * @method LinkContext getLinkContext()
  * @method mixed getStart()
+ * @method mixed getText()
  * @method mixed getType()
  * @method bool isEnd()
  * @method bool isId()
+ * @method bool isLinkContext()
  * @method bool isStart()
+ * @method bool isText()
  * @method bool isType()
  * @method setEnd(mixed $value)
  * @method setId(string $value)
+ * @method setLinkContext(LinkContext $value)
  * @method setStart(mixed $value)
+ * @method setText(mixed $value)
  * @method setType(mixed $value)
  */
 class Link extends AutoPropertyHandler
@@ -27,4 +33,9 @@ class Link extends AutoPropertyHandler
      */
     public $id;
     public $type;
+    public $text;
+    /**
+     * @var LinkContext
+     */
+    public $link_context;
 }
