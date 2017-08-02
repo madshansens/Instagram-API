3fd2a01be0bf35b72fdd0542ce18a2c621f5e440
mgp25 <me@mgp25.com>
2017-08-02 15:19:52 +0200

    Response/Model/PermanentItem: Add new properties

--- src/Response/Model/PermanentItem.php
+++ src/Response/Model/PermanentItem.php
@@ -7,19 +7,28 @@ use InstagramAPI\AutoPropertyHandler;
 /**
  * @method string getItemId()
  * @method mixed getItemType()
+ * @method mixed getLike()
+ * @method Link getLink()
  * @method Location getLocation()
+ * @method MediaData getMedia()
  * @method mixed getText()
  * @method mixed getTimestamp()
  * @method string getUserId()
  * @method bool isItemId()
  * @method bool isItemType()
+ * @method bool isLike()
+ * @method bool isLink()
  * @method bool isLocation()
+ * @method bool isMedia()
  * @method bool isText()
  * @method bool isTimestamp()
  * @method bool isUserId()
  * @method setItemId(string $value)
  * @method setItemType(mixed $value)
+ * @method setLike(mixed $value)
+ * @method setLink(Link $value)
  * @method setLocation(Location $value)
+ * @method setMedia(MediaData $value)
  * @method setText(mixed $value)
  * @method setTimestamp(mixed $value)
  * @method setUserId(string $value)
@@ -41,4 +50,13 @@ class PermanentItem extends AutoPropertyHandler
      * @var Location
      */
     public $location;
+    public $like;
+    /**
+     * @var MediaData
+     */
+    public $media;
+    /**
+     * @var Link
+     */
+    public $link;
 }
--- src/Response/Model/ReelShare.php
+++ src/Response/Model/ReelShare.php
@@ -6,7 +6,7 @@ use InstagramAPI\AutoPropertyHandler;
 
 /**
  * @method mixed getBroadcasts()
- * @method Media getMedia()
+ * @method Item getMedia()
  * @method mixed getStickerVersion()
  * @method mixed getStoryRankingToken()
  * @method mixed getText()
@@ -20,7 +20,7 @@ use InstagramAPI\AutoPropertyHandler;
  * @method bool isTray()
  * @method bool isType()
  * @method setBroadcasts(mixed $value)
- * @method setMedia(Media $value)
+ * @method setMedia(Item $value)
  * @method setStickerVersion(mixed $value)
  * @method setStoryRankingToken(mixed $value)
  * @method setText(mixed $value)
