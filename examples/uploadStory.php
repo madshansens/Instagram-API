2264418aadc488b63a30c5464d15f80a3956f08d
mgp25 <me@mgp25.com>
2017-08-04 19:18:29 +0200

    Forgot to run code style. Sorry

--- examples/uploadStory.php
+++ examples/uploadStory.php
@@ -30,23 +30,22 @@ try {
 //       or put an image overlay of the hashtag on top of your image.
 
 $metadata =
-    ['hashtags' =>
-        [
+    ['hashtags' => [
             // You can add more than one hashtag in the array.
             // All float values can be from 0.0 to 1.0.
             [
-                'tag_name' => '#test', // NOTE: This hashtag MUST appear in the caption.
-                'x' => 0.5, // x = 0.5 and y = 0.5 is center of screen.
-                'y' => 0.5,
-                'width' => 0.24305555,
-                'height' => 0.07347973,
-                'rotation' => 0.0,
-                'is_sticker' => false, // Don't change this value.
-                'use_custom_title' => false // Don't change this value.
+                'tag_name'         => '#test', // NOTE: This hashtag MUST appear in the caption.
+                'x'                => 0.5, // x = 0.5 and y = 0.5 is center of screen.
+                'y'                => 0.5,
+                'width'            => 0.24305555,
+                'height'           => 0.07347973,
+                'rotation'         => 0.0,
+                'is_sticker'       => false, // Don't change this value.
+                'use_custom_title' => false, // Don't change this value.
             ],
             // ...
         ],
-    'caption' => '#test this is a cool API'];
+    'caption' => '#test this is a cool API', ];
 
 try {
     $ig->story->uploadPhoto($photoFilename, $metadata);
