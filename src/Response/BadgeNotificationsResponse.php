6cc214ff4e2bef37bd5e88932a2b7f2aea0804d4
SteveJobzniak <SteveJobzniak@users.noreply.github.com>
2017-10-09 21:29:17 +0200

    Response/BadgeNotificationsResponse: Use container
    - Uses CoreUnpredictableContainer for now. We should define the actual
      value-data-type as a separate sub-container (like when we made
      UserUnpredictableContainer).

--- src/Response/BadgeNotificationsResponse.php
+++ src/Response/BadgeNotificationsResponse.php
@@ -5,7 +5,7 @@ namespace InstagramAPI\Response;
 use InstagramAPI\Response;
 
 /**
- * @method mixed getBadgePayload()
+ * @method Model\UnpredictableKeys\CoreUnpredictableContainer getBadgePayload()
  * @method string getMessage()
  * @method string getStatus()
  * @method Model\_Message[] get_Messages()
@@ -13,7 +13,7 @@ use InstagramAPI\Response;
  * @method bool isMessage()
  * @method bool isStatus()
  * @method bool is_Messages()
- * @method $this setBadgePayload(mixed $value)
+ * @method $this setBadgePayload(Model\UnpredictableKeys\CoreUnpredictableContainer $value)
  * @method $this setMessage(mixed $value)
  * @method $this setStatus(string $value)
  * @method $this set_Messages(Model\_Message[] $value)
@@ -25,7 +25,11 @@ use InstagramAPI\Response;
 class BadgeNotificationsResponse extends Response
 {
     const JSON_PROPERTY_MAP = [
-        // Only exists if you have notifications, contains data keyed by userId:
-        'badge_payload' => '',
+        // Only exists if you have notifications; contains data keyed by userId:
+        // TODO: Currently defined as a Core container, which means that all of
+        // the data sub-values will be arrays (no type-conversion of the values
+        // will happen). We should define this as a specific type (like
+        // UserUnpredictableContainer) when we know exactly what the data is.
+        'badge_payload' => 'Model\UnpredictableKeys\CoreUnpredictableContainer',
     ];
 }
