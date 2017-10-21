0f821fc8ee2d83936350620a189d5e40521e1b1f
SteveJobzniak <SteveJobzniak@users.noreply.github.com>
2017-10-21 23:00:47 +0200

    Utils: Improved atomicWrite() algorithm
    - Now verifies that 100% of the bytes were written to disk and will not
      treat anything as success unless 100% were written.
    
    - In case of any failure, it deletes the temp file if it still exists.

--- src/Utils.php
+++ src/Utils.php
@@ -1191,12 +1191,14 @@ class Utils
      * If the script is killed before the write is complete, only the temporary
      * trash file will be corrupted.
      *
+     * The algorithm also ensures that 100% of the bytes were written to disk.
+     *
      * @param string $filename     Filename to write the data to.
      * @param string $data         Data to write to file.
      * @param string $atomicSuffix Lets you optionally provide a different
      *                             suffix for the temporary file.
      *
-     * @return mixed Number of bytes written on success, otherwise FALSE.
+     * @return int|bool Number of bytes written on success, otherwise `FALSE`.
      */
     public static function atomicWrite(
         $filename,
@@ -1206,13 +1208,20 @@ class Utils
         // Perform an exclusive (locked) overwrite to a temporary file.
         $filenameTmp = sprintf('%s.%s', $filename, $atomicSuffix);
         $writeResult = @file_put_contents($filenameTmp, $data, LOCK_EX);
-        if ($writeResult !== false) {
-            // Now move the file to its real destination (replaced if exists).
+
+        // Only proceed if we wrote 100% of the data bytes to disk.
+        if ($writeResult !== false && $writeResult === strlen($data)) {
+            // Now move the file to its real destination (replaces if exists).
             $moveResult = @rename($filenameTmp, $filename);
             if ($moveResult === true) {
                 // Successful write and move. Return number of bytes written.
                 return $writeResult;
             }
+        }
+
+        // We've failed. Remove the temporary file if it exists.
+        if (is_file($filenameTmp)) {
+            @unlink($filenameTmp);
         }
 
         return false; // Failed.
