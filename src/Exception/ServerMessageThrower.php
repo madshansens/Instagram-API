ceccaf812696821fe05e9a56d73f73f5effff7f3
SteveJobzniak <SteveJobzniak@users.noreply.github.com>
2017-10-28 12:26:10 +0200

    ServerMessageThrower: Handle empty error messages
    Closes #1650.
    
    - Now handles empty `message` fields by displaying `error_type` as the
      message instead (if available), or otherwise `Request failed.` as a
      final generic fallback message if no better details are available
      whatsoever.
    
    - Marked `$serverMessage` as optional (`string|null`), since it comes
      from `getMessage()` which doesn't always have a value, so it has
      always been optional...
    
    - Reflowed all comments to 80 columns.
    
    - Now checks `ChallengeRequiredException` later than
      `CheckpointRequiredException` to avoid misdetections.
      See new code comments.

--- src/Exception/ServerMessageThrower.php
+++ src/Exception/ServerMessageThrower.php
@@ -28,21 +28,39 @@ class ServerMessageThrower
      * comment", "No permission to view profile" or other garbage. Those server
      * messages are human-readable, unreliable (they can change at any time) and
      * are also totally non-critical. You should handle them yourself in your
-     * end-user applications by simply catching their EndpointException and
-     * looking at the contents of its getMessage() property, or checking if it
-     * hasResponse() and then getResponse() to see the full server response. The
-     * exceptions listed below are *critical* exceptions related to the CORE of
-     * the API! Nothing else.
+     * end-user applications by simply catching their generic EndpointException
+     * and looking at the contents of its getMessage() property, or checking if
+     * it hasResponse() and then getResponse() to see the full server response.
+     * The exceptions listed below are *critical* exceptions related to the CORE
+     * of the API! Nothing else.
      *
      * @var array
      */
     const EXCEPTION_MAP = [
+        /*
+         * WARNING: We MUST be sure to list these exception messages in an order
+         * which guarantees that they will be properly detected without being
+         * detected as something else!
+         *
+         * For example, the "challenge_required" string ALSO exists inside of
+         * "checkpoint_challenge_required", so if we check for ChallengeRequired
+         * problems above CheckpointRequired, then we would ALWAYS detect
+         * checkpoints as "challenge required" since that string exists in both
+         * of them.
+         *
+         * Always list all exceptions in an order that guarantees that they
+         * cannot be misdetected as each other! The exceptions with the longest
+         * strings, in case of similar strings, MUST be checked/listed EARLIER!
+         *
+         * So in that example, CheckpointRequired MUST be listed above
+         * ChallengeRequired!
+         */
         'LoginRequiredException'       => ['login_required'],
-        'ChallengeRequiredException'   => ['challenge_required'],
         'CheckpointRequiredException'  => [
             'checkpoint_required', // message
             'checkpoint_challenge_required', // error_type
         ],
+        'ChallengeRequiredException'   => ['challenge_required'],
         'FeedbackRequiredException'    => ['feedback_required'],
         'IncorrectPasswordException'   => [
             // "The password you entered is incorrect".
@@ -72,14 +90,23 @@ class ServerMessageThrower
      *
      * Uses the generic EndpointException if no other exceptions match.
      *
-     * @param string|null                $prefixString   What prefix to use for the message in
-     *                                                   the final exception. Should be something
-     *                                                   helpful such as the name of the class or
-     *                                                   function which threw. Can be NULL.
-     * @param string                     $serverMessage  The failure string from Instagram's API.
-     * @param Response|null              $serverResponse The complete server response object,
-     *                                                   if one is available (optional).
-     * @param HttpResponseInterface|null $httpResponse   The HTTP response object (if available).
+     * @param string|null                $prefixString   What prefix to use for
+     *                                                   the message in the
+     *                                                   final exception. Should
+     *                                                   be something helpful
+     *                                                   such as the name of the
+     *                                                   class or function which
+     *                                                   threw. Can be `NULL`.
+     * @param string|null                $serverMessage  The failure string from
+     *                                                   Instagram's API (from
+     *                                                   `getMessage()`). Might
+     *                                                   be empty in some cases.
+     * @param Response|null              $serverResponse The complete server
+     *                                                   response object, if one
+     *                                                   is available
+     *                                                   (optional).
+     * @param HttpResponseInterface|null $httpResponse   The HTTP response
+     *                                                   object (if available).
      *
      * @throws InstagramException The appropriate exception.
      */
@@ -89,13 +116,16 @@ class ServerMessageThrower
         Response $serverResponse = null,
         HttpResponseInterface $httpResponse = null)
     {
+        // We will analyze both the `message` AND `error_type` (if available).
         $messages = [$serverMessage];
+        $serverErrorType = null;
         if ($serverResponse instanceof Response) {
             // We are reading a property that isn't defined in the class
             // property map, so we must use "has" first, to ensure it exists.
             if ($serverResponse->hasErrorType()
                 && is_string($serverResponse->getErrorType())) {
-                $messages[] = $serverResponse->getErrorType();
+                $serverErrorType = $serverResponse->getErrorType();
+                $messages[] = $serverErrorType;
             }
         }
 
@@ -145,14 +175,21 @@ class ServerMessageThrower
         // We need to specify the full namespace path to the exception class.
         $fullClassPath = '\\'.__NAMESPACE__.'\\'.$exceptionClass;
 
+        // Determine which message to display to the user.
+        $displayMessage = is_string($serverMessage) && strlen($serverMessage)
+                        ? $serverMessage : $serverErrorType;
+        if (!is_string($displayMessage) || !strlen($displayMessage)) {
+            $displayMessage = 'Request failed.';
+        }
+
         // Some Instagram messages already have punctuation, and others need it.
-        $serverMessage = self::prettifyMessage($serverMessage);
+        $displayMessage = self::prettifyMessage($displayMessage);
 
         // Create an instance of the final exception class, with the pretty msg.
         $e = new $fullClassPath(
             $prefixString !== null
-            ? sprintf('%s: %s', $prefixString, $serverMessage)
-            : $serverMessage
+            ? sprintf('%s: %s', $prefixString, $displayMessage)
+            : $displayMessage
         );
 
         // Attach the server response to the exception, IF a response exists.
