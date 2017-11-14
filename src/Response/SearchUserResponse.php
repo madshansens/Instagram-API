4d628e246a82c548d93fcfb25bb7d094537839e2
SteveJobzniak <SteveJobzniak@users.noreply.github.com>
2017-11-14 22:59:57 +0100

    Response/SearchUserResponse: MaxID has been removed

--- src/Response/SearchUserResponse.php
+++ src/Response/SearchUserResponse.php
@@ -9,7 +9,6 @@ use InstagramAPI\Response;
  *
  * @method bool getHasMore()
  * @method mixed getMessage()
- * @method string getNextMaxId()
  * @method int getNumResults()
  * @method string getRankToken()
  * @method string getStatus()
@@ -17,7 +16,6 @@ use InstagramAPI\Response;
  * @method Model\_Message[] get_Messages()
  * @method bool isHasMore()
  * @method bool isMessage()
- * @method bool isNextMaxId()
  * @method bool isNumResults()
  * @method bool isRankToken()
  * @method bool isStatus()
@@ -25,7 +23,6 @@ use InstagramAPI\Response;
  * @method bool is_Messages()
  * @method $this setHasMore(bool $value)
  * @method $this setMessage(mixed $value)
- * @method $this setNextMaxId(string $value)
  * @method $this setNumResults(int $value)
  * @method $this setRankToken(string $value)
  * @method $this setStatus(string $value)
@@ -33,7 +30,6 @@ use InstagramAPI\Response;
  * @method $this set_Messages(Model\_Message[] $value)
  * @method $this unsetHasMore()
  * @method $this unsetMessage()
- * @method $this unsetNextMaxId()
  * @method $this unsetNumResults()
  * @method $this unsetRankToken()
  * @method $this unsetStatus()
@@ -45,7 +41,6 @@ class SearchUserResponse extends Response
     const JSON_PROPERTY_MAP = [
         'has_more'    => 'bool',
         'num_results' => 'int',
-        'next_max_id' => 'string',
         'users'       => 'Model\User[]',
         'rank_token'  => 'string',
     ];
