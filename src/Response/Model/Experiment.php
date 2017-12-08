06cc981c6e80e4becbb0d346ea3d4522ad81b9a7
SteveJobzniak <SteveJobzniak@users.noreply.github.com>
2017-12-09 00:13:32 +0100

    Model/Experiment: Add new properties

--- src/Response/Model/Experiment.php
+++ src/Response/Model/Experiment.php
@@ -7,15 +7,19 @@ use InstagramAPI\AutoPropertyMapper;
 /**
  * Experiment.
  *
- * @method mixed getGroup()
- * @method mixed getName()
+ * @method mixed getAdditionalParams()
+ * @method string getGroup()
+ * @method string getName()
  * @method Param[] getParams()
+ * @method bool isAdditionalParams()
  * @method bool isGroup()
  * @method bool isName()
  * @method bool isParams()
- * @method $this setGroup(mixed $value)
- * @method $this setName(mixed $value)
+ * @method $this setAdditionalParams(mixed $value)
+ * @method $this setGroup(string $value)
+ * @method $this setName(string $value)
  * @method $this setParams(Param[] $value)
+ * @method $this unsetAdditionalParams()
  * @method $this unsetGroup()
  * @method $this unsetName()
  * @method $this unsetParams()
@@ -23,8 +27,9 @@ use InstagramAPI\AutoPropertyMapper;
 class Experiment extends AutoPropertyMapper
 {
     const JSON_PROPERTY_MAP = [
-        'params' => 'Param[]',
-        'group'  => '',
-        'name'   => '',
+        'name'              => 'string',
+        'group'             => 'string',
+        'additional_params' => '', // TODO: Only seen as [] empty array so far.
+        'params'            => 'Param[]',
     ];
 }
