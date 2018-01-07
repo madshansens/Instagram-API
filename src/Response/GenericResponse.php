d28ab4803843310f32e328e6742c05ba55d16555
mgp25 <me@mgp25.com>
2018-01-07 11:39:34 +0100

    /Response/GenericResponse: Added missing property

--- src/Response/GenericResponse.php
+++ src/Response/GenericResponse.php
@@ -9,19 +9,24 @@ use InstagramAPI\Response;
  *
  * @method mixed getMessage()
  * @method string getStatus()
+ * @method mixed getXsharingNonces()
  * @method Model\_Message[] get_Messages()
  * @method bool isMessage()
  * @method bool isStatus()
+ * @method bool isXsharingNonces()
  * @method bool is_Messages()
  * @method $this setMessage(mixed $value)
  * @method $this setStatus(string $value)
+ * @method $this setXsharingNonces(mixed $value)
  * @method $this set_Messages(Model\_Message[] $value)
  * @method $this unsetMessage()
  * @method $this unsetStatus()
+ * @method $this unsetXsharingNonces()
  * @method $this unset_Messages()
  */
 class GenericResponse extends Response
 {
     const JSON_PROPERTY_MAP = [
+        'xsharing_nonces' => '',
     ];
 }
