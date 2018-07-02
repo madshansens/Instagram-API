4515fdac0c6099c684b7e7fedb4aa3a95d99bb5c
Abyr Valg <valga@abyrga.ru>
2018-07-02 22:46:10 +0300

    Direct: Update thread item model

--- src/Response/Model/LiveVideoShare.php
+++ src/Response/Model/LiveVideoShare.php
@@ -7,12 +7,16 @@ use InstagramAPI\AutoPropertyMapper;
 /**
  * LiveVideoShare.
  *
+ * @method Broadcast getBroadcast()
  * @method string getText()
  * @method int getVideoOffset()
+ * @method bool isBroadcast()
  * @method bool isText()
  * @method bool isVideoOffset()
+ * @method $this setBroadcast(Broadcast $value)
  * @method $this setText(string $value)
  * @method $this setVideoOffset(int $value)
+ * @method $this unsetBroadcast()
  * @method $this unsetText()
  * @method $this unsetVideoOffset()
  */
@@ -20,6 +24,7 @@ class LiveVideoShare extends AutoPropertyMapper
 {
     const JSON_PROPERTY_MAP = [
         'text'                => 'string',
+        'broadcast'           => 'Broadcast',
         'video_offset'        => 'int',
     ];
 }
--- src/Response/Model/Product.php
+++ src/Response/Model/Product.php
@@ -8,32 +8,60 @@ use InstagramAPI\AutoPropertyMapper;
  * Product.
  *
  * @method string getCurrentPrice()
+ * @method string getDescription()
+ * @method string getExternalUrl()
  * @method string getFullPrice()
  * @method bool getHasViewerSaved()
+ * @method ProductImage getMainImage()
  * @method string getName()
  * @method string getPrice()
  * @method string getProductId()
+ * @method ProductImage[] getProductImages()
+ * @method string getReviewStatus()
+ * @method ProductImage getThumbnailImage()
  * @method bool isCurrentPrice()
+ * @method bool isDescription()
+ * @method bool isExternalUrl()
  * @method bool isFullPrice()
  * @method bool isHasViewerSaved()
+ * @method bool isMainImage()
  * @method bool isName()
  * @method bool isPrice()
  * @method bool isProductId()
+ * @method bool isProductImages()
+ * @method bool isReviewStatus()
+ * @method bool isThumbnailImage()
  * @method $this setCurrentPrice(string $value)
+ * @method $this setDescription(string $value)
+ * @method $this setExternalUrl(string $value)
  * @method $this setFullPrice(string $value)
  * @method $this setHasViewerSaved(bool $value)
+ * @method $this setMainImage(ProductImage $value)
  * @method $this setName(string $value)
  * @method $this setPrice(string $value)
  * @method $this setProductId(string $value)
+ * @method $this setProductImages(ProductImage[] $value)
+ * @method $this setReviewStatus(string $value)
+ * @method $this setThumbnailImage(ProductImage $value)
  * @method $this unsetCurrentPrice()
+ * @method $this unsetDescription()
+ * @method $this unsetExternalUrl()
  * @method $this unsetFullPrice()
  * @method $this unsetHasViewerSaved()
+ * @method $this unsetMainImage()
  * @method $this unsetName()
  * @method $this unsetPrice()
  * @method $this unsetProductId()
+ * @method $this unsetProductImages()
+ * @method $this unsetReviewStatus()
+ * @method $this unsetThumbnailImage()
  */
 class Product extends AutoPropertyMapper
 {
+    const APPROVED = 'approved';
+    const PENDING = 'pending';
+    const REJECTED = 'rejected';
+
     const JSON_PROPERTY_MAP = [
         'name'             => 'string',
         'price'            => 'string',
@@ -41,5 +69,11 @@ class Product extends AutoPropertyMapper
         'full_price'       => 'string',
         'product_id'       => 'string',
         'has_viewer_saved' => 'bool',
+        'description'      => 'string',
+        'main_image'       => 'ProductImage',
+        'thumbnail_image'  => 'ProductImage',
+        'product_images'   => 'ProductImage[]',
+        'external_url'     => 'string',
+        'review_status'    => 'string',
     ];
 }
