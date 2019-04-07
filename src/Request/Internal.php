a7bf7ff304bc255aac851607096ba807dd5fbdc7
JRoy <joshroy126@gmail.com>
2019-04-07 16:32:16 -0400

    Internal: Update & Compress markStoryMediaSeen()

--- src/Request/Internal.php
+++ src/Request/Internal.php
@@ -1358,6 +1358,7 @@ class Internal extends RequestCollection
      *                                        If NULL, we automatically use the
      *                                        user's profile ID from each Item
      *                                        object as the source ID.
+     * @param string                $module   Module where the story was found.
      *
      * @throws \InvalidArgumentException
      * @throws \InstagramAPI\Exception\InstagramException
@@ -1370,7 +1371,8 @@ class Internal extends RequestCollection
      */
     public function markStoryMediaSeen(
         array $items,
-        $sourceId = null)
+        $sourceId = null,
+        $module = 'feed_timeline')
     {
         // Build the list of seen media, with human randomization of seen-time.
         $reels = [];
@@ -1413,11 +1415,17 @@ class Internal extends RequestCollection
 
         return $this->ig->request('media/seen/')
             ->setVersion(2)
+            ->setIsBodyCompressed(true)
             ->addPost('_uuid', $this->ig->uuid)
             ->addPost('_uid', $this->ig->account_id)
             ->addPost('_csrftoken', $this->ig->client->getToken())
+            ->addPost('container_module', $module)
             ->addPost('reels', $reels)
+            ->addPost('reel_media_skipped', [])
             ->addPost('live_vods', [])
+            ->addPost('live_vods_skipped', [])
+            ->addPost('nuxes', [])
+            ->addPost('nuxes_skipped', [])
             ->addParam('reel', 1)
             ->addParam('live_vod', 0)
             ->getResponse(new Response\MediaSeenResponse());
