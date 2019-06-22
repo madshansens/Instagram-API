babad4f4f9a49d85b97cbdadbd587a072ab22c2f
JRoy <joshroy126@gmail.com>
2019-06-22 12:30:54 -0400

    Prepare for release

--- src/Instagram.php
+++ src/Instagram.php
@@ -3,7 +3,7 @@
 namespace InstagramAPI;
 
 /**
- * Instagram's Private API v5.
+ * Instagram's Private API v6.1.0.
  *
  * TERMS OF USE:
  * - This code is in no way affiliated with, authorized, maintained, sponsored
