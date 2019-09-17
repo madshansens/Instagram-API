53421f90b9ef7743f1c6221c4963f2b9f7a592e8
JRoy <joshroy126@gmail.com>
2019-09-16 20:56:42 -0400

    Instagram: Bump Version

--- src/Instagram.php
+++ src/Instagram.php
@@ -3,7 +3,7 @@
 namespace InstagramAPI;
 
 /**
- * Instagram's Private API v7.0.0.
+ * Instagram's Private API v7.0.1.
  *
  * TERMS OF USE:
  * - This code is in no way affiliated with, authorized, maintained, sponsored
