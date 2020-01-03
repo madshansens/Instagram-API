79bc6a7a7ccd13843b1943640c5e13d02d049231
JRoy <joshroy126@gmail.com>
2020-01-02 22:20:31 -0500

    Instagram: Implement encrypted password support

--- src/Instagram.php
+++ src/Instagram.php
@@ -502,6 +502,7 @@ class Instagram implements ExperimentsInterface
                     ->addPost('guid', $this->uuid)
                     ->addPost('device_id', $this->device_id)
                     ->addPost('password', $this->password)
+                    ->addPost('enc_password', Utils::encryptPassword($password, $this->settings->get('public_key_id'), $this->settings->get('public_key')))
                     ->addPost('google_tokens', '[]')
                     ->addPost('login_attempt_count', 0)
                     ->getResponse(new Response\LoginResponse());
@@ -949,7 +950,9 @@ class Instagram implements ExperimentsInterface
         $this->internal->bootstrapMsisdnHeader();
         $this->internal->readMsisdnHeader('default');
         $this->internal->syncDeviceFeatures(true);
-        $this->internal->sendLauncherSync(true);
+        $launcherResponse = $this->internal->sendLauncherSync(true)->getHttpResponse();
+        $this->settings->set('public_key', $launcherResponse->getHeaderLine('ig-set-password-encryption-pub-key'));
+        $this->settings->set('public_key_id', $launcherResponse->getHeaderLine('ig-set-password-encryption-key-id'));
         $this->internal->bootstrapMsisdnHeader();
         $this->internal->logAttribution();
         $this->account->getPrefillCandidates();
